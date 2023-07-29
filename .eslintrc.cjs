fdbc7a4e6ca3ebdb9ed940aff960072f90dbbff4
ainochi-kor <kthrkdals@gmail.com>
2023-07-29 16:37:48 +0900

    add: eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
   },
   extends: 'standard-with-typescript',
-  plugins: ["no-console"],
+  plugins: ['@typescript-eslint'],
   overrides: [
     {
       env: {
@@ -12,17 +12,22 @@ module.exports = {
       },
       files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: 'script',
-        project: './tsconfig.js',
+        project: './tsconfig.json',
       },
     },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    project: './tsconfig.json', // 여기에 추가합니다.
   },
   rules: {
-    "no-console": ["error", { allow: ["warn", "error"] }]
+    'no-console': ['error', { allow: ['warn', 'error'] }], // console에서 log는 에러, error는 경고
+    "semi": "off", // 세미콜론 붙이면 에러 (err)
+    "@typescript-eslint/semi": "off", // 세미콜론 붙이면 에러 (err)
+    "@typescript-eslint/no-non-null-assertion": "off", // null이 아닌 assertion 에러 표시 (off)
+    "space-before-function-paren": "off", // 함수 앞 공백이 없으면 에러 (off)
+    "@typescript-eslint/space-before-function-paren": "off" // 함수 앞 공백이 없으면 에러 (off)
   },
   ignorePatterns: ['.eslintrc.cjs'],
 };
